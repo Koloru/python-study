89e7053b474749ba997e6c3ffe347ae078007c45
Koloru <sos.llyr@gmail.com>
2023-01-25 02:04:37 +0800

    Learning how to make user input data into a variable

--- 3. UserInput.py
+++ 3. UserInput.py
@@ -14,5 +14,5 @@
 length = int(input("Enter the length of the rectangle"))
 width= int(input("Enter the width of the rectangle"))
 
-area =length*width
+area = length*width
 print(f"Area is {area}")
