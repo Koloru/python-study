5d595dfb2fe102a6ac588b2e00b1d45ef5d41de8
Koloru <sos.llyr@gmail.com>
2023-01-26 04:22:12 +0800

    string methods in python

---
+++ 7. StringMethods.py
@@ -0,0 +1,39 @@
+
+name = "Neil"
+
+# Length of a string returns an integer
+len(name)
+
+
+# find - finds the first occurrence of the parameter and returns the index
+name.find("l") # finds the first occurence of l
+
+# rfind - finds the last occurrence of the parameter and returns the index
+name.find("l")
+
+
+# capitalize, upper, lower
+name.capitalize()
+name.upper()
+name.lower()
+
+# returns true of false if all the characters are digits
+name.isdigit()
+
+# contains alphabetical characters
+# spaces are non alphabetic
+# numbers are not alphabetic
+name.isalpha()
+
+
+phone_number = "09773900529"
+
+phone_number.count("7")
+
+# replace 0 with 1
+phone_number.replace("0", "1")
+
+
+
+
+
