3061d4db98bb52f092e1199269d1302c6ce6b12b
Koloru <sos.llyr@gmail.com>
2023-01-30 03:53:27 +0800

    add comments

--- Exercises/CreditCardValidator.py
+++ Exercises/CreditCardValidator.py
@@ -1,4 +1,14 @@
 
+# Python credit card validator program
+
+# 1. Remove any '-' or ' '
+# 2. Add all digits in the odd places from right to left
+# 3. Double every second digit from right to left.
+#        (If result is a two-digit number,
+#        add the two-digit number together to get a single digit.)
+# 4. Sum the totals of steps 2 & 3
+# 5. If sum is divisible by 10, the credit card # is valid
+
 sum_odd_digits = 0
 sum_even_digits = 0
 total = 0
@@ -14,22 +24,27 @@ card_number = card_number.replace(" ", "")
 # Reverse card number / Reverse String
 card_number = card_number[::-1]
 
-
+# add all digits in tge odd places from right to left
 for x in card_number[::2]:
     sum_odd_digits += int(x)
     # Step 2
 
+# Double every second digit from right to left
 for x in card_number[1::2]:
     x = int(x)*2
+    # If result is a two-digit number,
+    # add the two-digit number together to get a single digit.
     if x >= 10:
         sum_even_digits += (1 + (x % 10))
     else:
         sum_even_digits += x
         
 
-
+# Sum the totals of steps 2 & 3
 total = sum_even_digits + sum_odd_digits
 
+
+# 5. If sum is divisible by 10, the credit card # is valid
 if total % 10 == 0:
     print("Valid card number")
 else:
