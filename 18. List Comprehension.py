d818a8fe09fa498e172c55b79a035b2d453b9695
Koloru <sos.llyr@gmail.com>
2023-10-11 08:03:28 +0800

    List comprehension 2

--- 18. List Comprehension.py
+++ 18. List Comprehension.py
@@ -16,7 +16,21 @@ print(squares)
 # and can also be rewritten as
 # Syntax is
 # list_name = [(expression/condition) for ITEM in ITERABLE]
-squares = [add(i) for i in range(1, 11)]
-squares = [(i+i*i) for i in range(1, 11)]
 
+# The if conditional is optional
+# list_name = [(expression/condition) for ITEM in ITERABLE if conditional]
+squares = [add(i) for i in range(1, 11) ]
+
+# Add it to the last if i is greater than 2
+squares = [(i+i) for i in range(1, 11) if i > 2]
 print(squares)
+
+# adding more than 1 conditional the formula syntax changes
+# it turns into
+# list_name = [(expression/condition) (if/else) for ITEM in ITERABLE]
+
+# Read as add i+i if i is greater than 2 else make it failed in the range of 1,11
+squares = [(i+i) if i > 2 else "failed" for i in range(1, 11) ]
+print(squares)
+
+# Using list comprehension to mimic lambda functions
